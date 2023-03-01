b130d9399ed7846c7548431adeaddf0263fb8718
leviscop <git@leviscop.dev>
2023-03-01 16:42:51 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -24,14 +24,12 @@ case $OS in
     echo "OS unsupported! Exiting..";
     exit 0;;
 esac
-while true; do
-    read -p "Do you want to change the root password? " yn
-    case $yn in
-        [Yy]* ) passwd;;
-        [Nn]* ) break;;
-        * ) break;;
-    esac
-done
+read -p "Do you want to change the root password? " yn
+case $yn in
+    [Yy]* ) passwd;;
+    [Nn]* ) echo -n "";;
+    * ) echo -n "";;
+esac
 case $OS in
     "Alpine Linux")
         echo "Updating system..";
@@ -45,21 +43,19 @@ case $OS in
         apt-get -qq update &>/dev/null && DEBIAN_FRONTEND=noninteractive DEBIAN_PRIORITY=critical apt-get -q -y -o "Dpkg::Options::=--force-confdef" -o "Dpkg::Options::=--force-confold" upgrade &>/dev/null;
         echo "Installing basic packages..";
         DEBIAN_FRONTEND=noninteractive DEBIAN_PRIORITY=critical apt-get -q -y -o "Dpkg::Options::=--force-confdef" -o "Dpkg::Options::=--force-confold" install apt-transport-https ca-certificates curl gnupg lsb-release bind9-dnsutils figlet &>/dev/null;;
-    * ) :;;
+    * ) echo -n "";;
 esac
-while true; do
-    echo "Hostname is $(hostname -f)"
-    read -p "Do you want to change the hostname? " yn
-    case $yn in
-        [Yy]* ) read -p "Enter a hostname: " hostname;
-        echo "Setting hostname..";
-        echo "$hostname" > /etc/hostname;
-        hostname -F /etc/hostname;
-        echo "New hostname is $(hostname -f)";;
-        [Nn]* ) break;;
-        * ) break;;
-    esac
-done
+echo "Hostname is $(hostname -f)"
+read -p "Do you want to change the hostname? " yn
+case $yn in
+    [Yy]* ) read -p "Enter a hostname: " hostname;
+    echo "Setting hostname..";
+    echo "$hostname" > /etc/hostname;
+    hostname -F /etc/hostname;
+    echo "New hostname is $(hostname -f)";;
+    [Nn]* ) echo -n "";;
+    * ) echo -n "";;
+esac
 case $OS in
     "Alpine Linux" )
         echo "Welcome to $OS!" > /etc/motd
@@ -83,19 +79,17 @@ case $OS in
         echo "Installing docker-compose..";
         curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose &>/dev/null;
         chmod +x /usr/local/bin/docker-compose &>/dev/null;;
-    * ) :;;
+    * ) echo -n "";;
 esac
 echo "Running basic containers.."
 docker run -d --name ipv6nat --cap-drop ALL --cap-add NET_ADMIN --cap-add NET_RAW --cap-add SYS_MODULE --network host --restart unless-stopped -v /var/run/docker.sock:/var/run/docker.sock:ro -v /lib/modules:/lib/modules:ro robbertkl/ipv6nat &>/dev/null
 docker run -d -p 9001:9001 --name portainer_agent --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v /var/lib/docker/volumes:/var/lib/docker/volumes portainer/agent:latest &>/dev/null
-while true; do
-    read -p "Enable monitoring? " yn
-    case $yn in
-        [Yy]* ) mkdir -p /volume/dem &>/dev/null; curl -s https://raw.githubusercontent.com/leviscop/init-script/main/dem.conf -o /volume/dem/conf.yml &>/dev/null; read -p "Discord webhook url: " webhook; sed -i "s/<hostname>/$(hostname -s)/g" /volume/dem/conf.yml; sed -i "s#<webhook>#$webhook#g" /volume/dem/conf.yml; docker run -d --name dem --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v /volume/dem/conf.yml:/app/conf.yml quaide/dem:latest &>/dev/null;;
-        [Nn]* ) break;;
-        * ) break;;
-    esac
-done
+read -p "Enable monitoring? " yn
+case $yn in
+    [Yy]* ) mkdir -p /volume/dem &>/dev/null; curl -s https://raw.githubusercontent.com/leviscop/init-script/main/dem.conf -o /volume/dem/conf.yml &>/dev/null; read -p "Discord webhook url: " webhook; sed -i "s/<hostname>/$(hostname -s)/g" /volume/dem/conf.yml; sed -i "s#<webhook>#$webhook#g" /volume/dem/conf.yml; docker run -d --name dem --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v /volume/dem/conf.yml:/app/conf.yml quaide/dem:latest &>/dev/null;;
+    [Nn]* ) echo -n "";;
+    * ) echo -n "";;
+esac
 echo "Creating basic networks.."
 docker network create -d bridge --ipv6 --subnet fd00:172:20::/48 --gateway fd00:172:20::1 --subnet 172.20.0.0/16 --gateway 172.20.0.1 --attachable gateway &>/dev/null
 docker network create -d bridge --ipv6 --subnet fd00:172:24::/48 --gateway fd00:172:24::1 --subnet 172.24.0.0/16 --gateway 172.24.0.1 --attachable database &>/dev/null
@@ -107,12 +101,10 @@ touch /volume/proxy/acme-$(hostname -s)-tls.json && chmod 600 /volume/proxy/acme
 sysctl -w net.core.rmem_max=2500000 &>/dev/null
 echo "Done!"
 echo "You can add this environment to portainer with the address: $(hostname -f):9001"
-while true; do
-    read -p "A system restart could be required. Do you want to restart the system? " yn
-    case $yn in
-        [Yy]* ) reboot;;
-        [Nn]* ) exit 0;;
-        * ) exit 0;;
-    esac
-done
+read -p "A system restart could be required. Do you want to restart the system? " yn
+case $yn in
+    [Yy]* ) reboot;;
+    [Nn]* ) exit 0;;
+    * ) exit 0;;
+esac
 exit 0
